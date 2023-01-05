ee8718b63f7eaa416fe40ca5fdf17dd6f8f6e042
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-05 18:41:09 +0900

    :white_check_mark: JDK 동적 프록시 예제 테스트 코드

---
+++ proxy/src/test/java/hello/proxy/jdkdynamic/code/TimeInvocationHandler.java
@@ -0,0 +1,32 @@
+package hello.proxy.jdkdynamic.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * JDK 동적 프록시에 적용할 공통 로직
+ */
+@Slf4j
+public class TimeInvocationHandler implements InvocationHandler {
+
+    private final Object target;   // 동적 프록시가 호출할 대상
+
+    public TimeInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {  // method : call
+        log.info("Time Proxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = method.invoke(target, args);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
