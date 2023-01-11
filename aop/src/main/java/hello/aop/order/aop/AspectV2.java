c8f4e413a83d19f656db8b4ca89f4cfe53d04fe0
semi-cloud <sksmsrkdtpal@naver.com>
2023-01-11 16:51:54 +0900

    :sparkles: 스프링 AOP 구현 2: 포인트컷 분리
    포인트컷의 분리로 하나의 포인트컷을 여러 어드바이스에 적용 가능(모듈화)

---
+++ aop/src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,22 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+public class AspectV2 {
+
+    /**
+     * 주문과 관련된 모든 기능을 대상으로 하는 포인트 컷
+     */
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder(){}      // pointCut Signature(메서드 이름 + 파라미터)
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {  // Advice
+        log.info("[log] {}", joinPoint.getSignature());   // 메서드의 정보
+        return joinPoint.proceed();
+    }
+}
